8562cd602a691b1a3bf123c7219e8c0b258555de
Taylor Putnam <putntayl@isu.edu>
2021-01-26 21:13:28 -0700

    Started to work on player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = .5f;
+
+    private Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+        var movement = new Vector2(moveHorizontal, moveVertical) * speed;
+        transform.position = new Vector3(transform.position.x + movement.x, transform.position.y + movement.y);
+    }
+}
