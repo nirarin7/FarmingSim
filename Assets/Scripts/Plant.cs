9dfb98069d2715f760de2886be3eebd0cf992db4
nirarin7 <estearin@isu.edu>
2021-01-27 20:25:20 -0700

    Added plant object

---
+++ Assets/Scripts/Plant.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plant : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
