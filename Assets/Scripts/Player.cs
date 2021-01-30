eb058d4b3160d720783dab6d3cc84e36490706bb
Taylor Putnam <putntayl@isu.edu>
2021-01-30 15:40:56 -0700

    Added player animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,24 +7,30 @@ public class Player : MonoBehaviour
 {
     public float speed = .5f;
 
-    private Rigidbody2D rigidbody;
+
+    private Rigidbody2D _rigidbody;
+    private Animator _animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody2D>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     private void FixedUpdate()
     {
         var moveHorizontal = Input.GetAxis("Horizontal");
         var moveVertical = Input.GetAxis("Vertical");
+
+        _animator.SetFloat("HorizontalDirection", moveHorizontal);
+        _animator.SetFloat("VerticalDirection", moveVertical);
+        
         var movement = new Vector2(moveHorizontal, moveVertical) * speed;
         transform.position = new Vector3(transform.position.x + movement.x, transform.position.y + movement.y);
     }
